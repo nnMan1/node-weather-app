ca64e6c0f5a7598480aedb82b30b9a4ec455631d
Velibor Došljak <velibor.dosljak@fleka.me>
2020-01-17 13:10:24 +0100

    Adds postgres database client

---
+++ src/database.js
@@ -0,0 +1,19 @@
+const {Pool, Client} = require('pg')
+const connectionString = 'postgressql://postgres:natasa12\@@localhost:5432/testDB'
+
+const client = new Client({
+    connectionString: connectionString,
+
+})
+
+client.connect()
+
+client.query('insert into tablea1(id,name) values (1,\'Velibor Dosljak2\')' , (err,res) => {
+    console.log(err,res)
+    client.end()
+})
+
+//client.query('select * from tablea1', (err,res) => {
+//    console.log(err,res)
+//    client.end()
+//})

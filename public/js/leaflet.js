e8c60fd4a5499a98bcd0df2584244c90633fdedc
Velibor <dosljakvelbior@gmail.com>
2020-01-24 00:14:30 +0100

    Adds radio buttons for selecting what you want to add

--- public/js/leaflet.js
+++ public/js/leaflet.js
@@ -1,26 +1,76 @@
-var map = L.map('map').setView([42.77524, 19.42383], 8.2);
+var map = L.map('map').setView([42.77524, 19.42383], 9);
       var gl = L.mapboxGL({
         attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a>',
         accessToken: 'not-needed',
         style: 'https://api.maptiler.com/maps/b70c705e-6b80-4cc6-8718-0e061c056073/style.json?key=fuDms8pP4WajMwiCl9A9'
       }).addTo(map);
 
+var stubovi = [];
+var stuboviMarkers = L.layerGroup().addTo(map);
+var vodovi = [];
+var trafostanice = [];
+var gradovi = [];
+var selectedValue = undefined
 
-var popLocation= new L.LatLng(42.77524, 19.42383);
-var popup = L.popup()
-    .setLatLng(popLocation)
-    .setContent('<p>Hello world!<br />This is a nice popup.</p>')
-    .openOn(map);
+
+
+
+// var popLocation= new L.LatLng(42.77524, 19.42383);
+// var popup = L.popup()
+//     .setLatLng(popLocation)
+//     .setContent('<p>Hello world!<br />This is a nice popup.</p>')
+//     .openOn(map);
 
 map.on('click', function(e) {        
         var popLocation= e.latlng;
-        $.ajax('http://localhost:3001/stub', {
+       
+        switch (selectedValue) {
+            case "Stub" :
+                addStub({
+                            geo_duzina: popLocation.lat,
+                            geo_sirina: popLocation.lng,
+                            stanje_id: 1
+                        });
+                    break;
+            default:
+                    break;
+        }   
+              
+    });
+
+
+//show all stubovi
+function showAllStubovi () {
+    $.ajax('/stub', {
+        data: {
+            
+        }
+    })
+    .then(
+        function success(stubovi) {
+            stubovi.forEach(stub => {
+                let marker = L.marker(stub.geometry.coordinates).addTo(map)
+                stuboviMarkers.addLayer(marker);
+            });
+    
+            var overlay = {'markers': stuboviMarkers};
+            L.control.layers(null, overlay).addTo(map);
+            zoom_based_layerchange()
+        },
+    
+        function fail(data, status) {
+            alert('Request failed.  Returned status of ' + status);
+        }
+    );
+}
+
+function addStub (data) {
+
+    document.getElementById("map").style.cursor = "move";
+
+    $.ajax('http://localhost:3001/stub', {
             method: 'POST',
-            data: {
-                geo_duzina: popLocation.lat,
-                geo_sirina: popLocation.lng,
-                stanje_id: 1
-            }
+            data: data
         })
         .then(
             function success(name) {
@@ -32,5 +82,43 @@ map.on('click', function(e) {
             function fail(data, status) {
                 alert('Request failed.  Returned status of ' + status);
             }
-        );      
-    });
+        );
+}
+
+
+map.on('zoomend', function (e) {
+    zoom_based_layerchange();
+});
+
+
+function zoom_based_layerchange() {
+    //console.log(map.getZoom());
+
+var currentZoom = map.getZoom();
+    if (currentZoom < 17) {
+        map.removeLayer(stuboviMarkers);
+    } else {
+        map.addLayer(stuboviMarkers);
+    }
+}
+
+
+var addElementTypeRadioButtons = document.getElementsByName('elementToAdd') 
+
+addElementTypeRadioButtons.forEach(radioButton => {
+    radioButton.onclick = (e) => {
+        selectedValue = e.target.value
+        console.log(e.target.value)
+        addElementButton.disabled = false;
+    }
+});
+
+
+var addElementButton = document.getElementById("addElementButton");
+addElementButton.disabled = true;
+addElementButton.onclick = () => {
+    document.getElementById("map").style.cursor = "crosshair";
+}
+
+
+showAllStubovi()
